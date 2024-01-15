95871f62eaaa1a25dabd0935dcd8b8e0d525db2d
Gunther Weissenbaeck <weissenbaeck@posteo.de>
2024-01-15 20:22:01 +0100

    pictures app

---
+++ app/pictures/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from .views import (
+    pictures_all,
+    pictures_single,
+    pictures_edit,
+    pictures_delete,
+)
+
+urlpatterns = [
+    path("all/", pictures_all, name="pictures_all"),
+    path("single/<id>", pictures_single, name="pictures_single"),
+    path("edit/<id>", pictures_edit, name="pictures_edit"),
+    path("delete/<id>", pictures_delete, name="pictures_delete"),
+]
--- app/pictures/views.py
+++ app/pictures/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Picture
+from bird.models import FallenBird
+
+
+def pictures_all(request):
+    pictures = Picture.objects.all()
+    context = {"pictures": pictures}
+    return render(request, "pictures/pictures_all.html", context)
+
+
+def pictures_single(request, id):
+    pictures = Picture.objects.all().filter(fallenbird=id)
+    bird = FallenBird.objects.get(id=id)
+    if not pictures:
+        pictures = []
+    context = {"pictures": pictures, "bird": bird}
+    return render(request, "pictures/pictures_single.html", context)
+
+
+def pictures_edit(request, id):
+    picture = Picture.objects.all().get(id=id)
+    context = {"picture": picture}
+    return render(request, "pictures/pictures_edit.html", context)
+
+
+def pictures_delete(request, id):
+    picture = Picture.objects.all().get(id=id)
+    context = {"picture": picture}
+    return render(request, "pictures/pictures_delete.html", context)
